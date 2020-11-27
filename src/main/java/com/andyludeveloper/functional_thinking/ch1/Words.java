728ab641f09d038566e80ce6109ce59537b32d04
AndyLu <andyludeveloper@gmail.com>
2020-11-27 14:23:57 +0800

    Implement wordFreq in FP

--- src/main/java/com/andyludeveloper/functional_thinking/ch1/Words.java
+++ src/main/java/com/andyludeveloper/functional_thinking/ch1/Words.java
@@ -1,9 +1,6 @@
 package com.andyludeveloper.functional_thinking.ch1;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -44,5 +41,21 @@ public class Words {
             }
         }
         return wordMap;
+    }
+
+    public Map wordFreqFP(String words){
+        TreeMap<String, Integer> wordMap = new TreeMap<>();
+        regexToList(words, "\\w+").stream()
+                .map(w -> w.toLowerCase())
+                .filter(w -> !NON_WORDS.contains(w))
+                .forEach(w -> wordMap.put(w, wordMap.getOrDefault(w, 0)+1));
+        return wordMap;
+    }
+
+    private List<String> regexToList(String words, String regex){
+        List wordList = new ArrayList();
+        Matcher m = Pattern.compile(regex).matcher(words);
+        while(m.find())wordList.add(m.group());
+        return wordList;
     }
 }
